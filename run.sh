366ed074a5b18314c584d939be198db994453836
Alexey Noskov <alexey.noskov@gmail.com>
2016-11-13 23:24:58 +0300

    Initial run script

---
+++ run.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+prepare() {
+  echo "Preparing $1..."
+  python prepare-$1.py
+}
+
+train() {
+  echo "Training $1..."
+  time python -u train-$1.py | tee logs/$1.log
+}
+
+
+# Prepare data
+prepare basic
+prepare targets
+prepare products
+
+# Basic models
+train hashes
