f79d7fca7983ad4a88be42bafa73eced79fad6df
Alexey Noskov <alexey.noskov@gmail.com>
2016-12-18 22:26:51 +0300

    More lags

--- prepare-feature-lags.py
+++ prepare-feature-lags.py
@@ -5,17 +5,22 @@ from meta import train_dates, test_date
 
 from util import Dataset
 
-n_lags = 3
+n_lags = 4
 
-feature_columns = ['tiprel_1mes', 'indrel_1mes', 'age', 'segmento']
+feature_columns = ['tiprel_1mes', 'indrel_1mes', 'age', 'segmento', 'province_code']
 res_columns = ["%s_lag_%d" % (col, lag) for lag in xrange(1, n_lags+1) for col in feature_columns]
+
+src_parts = ['manual', 'province']
+src_columns = sum(map(Dataset.get_part_features, src_parts), [])
 
 past_features = []
 
 for di, dt in enumerate(train_dates + [test_date]):
     print "Processing %s..." % dt
 
-    cur = pd.DataFrame(Dataset.load_part(dt, 'manual'), columns=Dataset.get_part_features('manual'), index=Dataset.load_part(dt, 'idx'))
+    cur = np.hstack([Dataset.load_part(dt, p) for p in src_parts])
+    cur = pd.DataFrame(cur, columns=src_columns, index=Dataset.load_part(dt, 'idx'))
+
     df = pd.DataFrame(-110, columns=res_columns, index=cur.index, dtype=np.float32)
 
     for lag in xrange(1, n_lags+1):
--- prepare-product-lags.py
+++ prepare-product-lags.py
@@ -7,11 +7,38 @@ from meta import train_dates, test_date, product_columns
 
 from util import Dataset
 
-n_lags = 4
+product_lag_columns = [
+    'ind_ahor_fin_ult1',
+    'ind_aval_fin_ult1',
+    'ind_cco_fin_ult1',
+    'ind_cder_fin_ult1',
+    'ind_cno_fin_ult1',
+    'ind_ctju_fin_ult1',
+    'ind_ctma_fin_ult1',
+    'ind_ctop_fin_ult1',
+    'ind_ctpp_fin_ult1',
+    'ind_deco_fin_ult1',
+    #'ind_deme_fin_ult1',
+    'ind_dela_fin_ult1',
+    'ind_ecue_fin_ult1',
+    'ind_fond_fin_ult1',
+    #'ind_hip_fin_ult1',
+    'ind_plan_fin_ult1',
+    'ind_pres_fin_ult1',
+    'ind_reca_fin_ult1',
+    'ind_tjcr_fin_ult1',
+    'ind_valo_fin_ult1',
+    #'ind_viv_fin_ult1',
+    'ind_nomina_ult1',
+    'ind_nom_pens_ult1',
+    'ind_recibo_ult1'
+]
+
+n_lags = 5
 
 past_usage = []
 
-res_columns = ["%s_lag_%d" % (col, lag) for lag in xrange(1, n_lags+1) for col in product_columns]
+res_columns = ["%s_lag_%d" % (col, lag) for lag in xrange(1, n_lags+1) for col in product_lag_columns]
 
 for di, dt in enumerate(train_dates + [test_date]):
     print "Processing %s..." % dt
@@ -24,7 +51,7 @@ for di, dt in enumerate(train_dates + [test_date]):
         if di - lag >= 0:
             idx = index.intersection(past_usage[di-lag].index)
 
-            for col in product_columns:
+            for col in product_lag_columns:
                 df.loc[idx, "%s_lag_%d" % (col, lag)] = past_usage[di-lag].loc[idx, col]
 
     Dataset.save_part(dt, 'product-lags', sp.csr_matrix(df.values))
